fbf0acb4fe4bc0a9e54589dc31e8de436057b4ff
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2022-03-31 17:03:43 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,34 +7,41 @@
  import {Wrapper} from './App.styles'
 
 function App() {
-  const [contacts, setContacts]=useState(
-    ()=>JSON.parse(window.localStorage.getItem('contacts')) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  );
-  const [filter, setFilter]=useState('');
+//   const [contacts, setContacts] = useState([]);
+//   const [filter, setFilter] = useState ('');
 
-  useEffect(()=>{
-    window.localStorage.setItem('contacts', JSON.stringify(contacts))
-  },[contacts]);
+//   useEffect(() => {
+//   const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+//   if (savedContacts) setContacts(savedContacts);
+// }, []);
+const [contacts, setContacts] = useState(()=>{
+  return JSON.parse(localStorage.getItem("contacts")) }
+  
+);
+const [filter, setFilter] = useState ('');
+
+ useEffect(() => {
+  window.localStorage.setItem("contacts", JSON.stringify(contacts));
+}, [contacts]); 
+  
 
   const filterContacts = () => {
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.toLowerCase())
-    );
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()),)
   };
-  const addContact = ({ name, number }) => {
+  const addContact = ({ name, tel })=>{
     const contact = {
-      id: nanoid(),
       name,
-      number,
+      tel,
+      id: nanoid(),
     };
-    contacts.some((i) => i.name === name)
-      ? alert(`${name} is already in contacts`)
-      : setContacts([contact, ...contacts]);
+
+    if (contacts.find((item) => item.name === contact.name)) {
+      alert(`${name} is already in contacts`)
+      return;
+    }
+
+    setContacts([contact, ...contacts])
   };
   // const addContact = ({name,number }) => {
   //       const contact = {
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,23 +1,25 @@
-import { List, Text, Button } from './ContactList.styles';
-import PropTypes from 'prop-types';
-const ContactList = ({ items, onDeleteContact }) => (
-  <List>
-    {items.map(contact => (
-      <li key={contact.id} className="listStyle">
-        <Text className="TodoList__text">
-          {contact.name}:{contact.number}
-        </Text>
-        <Button
-          type="button"
-          className="TodoList__btn"
-          onClick={() => onDeleteContact(contact.id)}
-        >
-          Delete
-        </Button>
-      </li>
-    ))}
-  </List>
-);
+import { List, Text, Button } from "./ContactList.styles";
+import PropTypes from "prop-types";
+const ContactList = ({ items, onDeleteContact }) => {
+  return (
+    <List>
+      {items.map(({ id, name, number }) => (
+        <li key={id} className="listStyle">
+          <Text className="TodoList__text">
+            {name}:{number}
+          </Text>
+          <Button
+            type="button"
+            className="TodoList__btn"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete
+          </Button>
+        </li>
+      ))}
+    </List>
+  );
+};
 ContactList.prototype = {
   items: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   onDeleteContact: PropTypes.func.isRequired,
